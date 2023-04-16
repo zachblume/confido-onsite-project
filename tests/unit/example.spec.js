1f3a40585c51bf5498fa73730ac4ed15067ceb5b
zachblume <zachblume@gmail.com>
2023-04-15 21:49:57 -0400

    prettier

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,7 +2,7 @@ function sum(a, b) {
   return a + b;
 }
 describe("sum", () => {
-  test('adds 1 + 2 to equal 3', () => {
+  test("adds 1 + 2 to equal 3", () => {
     expect(sum(1, 2)).toBe(3);
   });
 });
