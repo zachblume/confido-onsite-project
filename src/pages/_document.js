04e5898ebc35bac3d733baf7e0753cda643cdaf8
zachblume <zachblume@gmail.com>
2023-04-16 16:33:40 -0400

    styling

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
     return (
-        <Html lang="en" class="h-full ">
+        <Html lang="en" className="h-full ">
             <Head />
-            <body class="h-full">
+            <body className="h-full">
                 <Main />
                 <NextScript />
             </body>
