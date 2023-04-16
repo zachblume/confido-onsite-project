f89babce497180f37d570d0cd32ec40329af635f
zachblume <zachblume@gmail.com>
2023-04-15 21:46:13 -0400

    add playwright

--- jest.config.mjs
+++ jest.config.mjs
@@ -11,8 +11,8 @@ const createJestConfig = nextJest({
 const config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-
   testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['<rootDir>/tests/unit/*.{js,jsx,ts,tsx}'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
