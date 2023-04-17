d05009e72dbc60a48ad74690fb78d76873e7c038
zachblume <zachblume@gmail.com>
2023-04-17 08:47:04 -0400

    refactor form onsubmit

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,10 @@ const Home = () => {
         (await postgrest.from("todos").update({ done: true }).match({ id })) && mutate();
     const deleteTodo = async (id) =>
         (await postgrest.from("todos").delete().match({ id })) && mutate();
+    const insertFormHandler = (event) => {
+        event.preventDefault();
+        insert(event.target.task.value);
+    };
 
     return (
         <div className="mx-auto max-w-7xl px-4 lg:px-8 main my-10">
@@ -20,12 +24,7 @@ const Home = () => {
                     <button onClick={() => deleteTodo(todo.id)}>Delete</button>
                 </div>
             ))}
-            <form
-                onSubmit={(event) => {
-                    event.preventDefault();
-                    insert(event.target.task.value);
-                }}
-            >
+            <form onSubmit={insertFormHandler}>
                 <input type="text" name="task" />
                 <button type="submit">New Todo</button>
             </form>
