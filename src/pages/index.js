0728d189297c9f56a64acd3833214d144bcd9b06
zachblume <zachblume@gmail.com>
2023-04-17 08:46:03 -0400

    simpler css

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,28 +9,26 @@ const Home = () => {
         (await postgrest.from("todos").delete().match({ id })) && mutate();
 
     return (
-        <div className="py-10">
-            <div className="mx-auto max-w-7xl px-4 lg:px-8 main">
-                <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 mb-3">
-                    Todos
-                </h1>
-                {todos?.map((todo) => (
-                    <div className={"todo " + (todo?.done && "line-through")} key={todo.id}>
-                        {todo.task}
-                        <button onClick={() => markDone(todo.id)}>Mark done</button>
-                        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-                    </div>
-                ))}
-                <form
-                    onSubmit={(event) => {
-                        event.preventDefault();
-                        insert(event.target.task.value);
-                    }}
-                >
-                    <input type="text" name="task" />
-                    <button type="submit">New Todo</button>
-                </form>
-            </div>
+        <div className="mx-auto max-w-7xl px-4 lg:px-8 main my-10">
+            <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 mb-3">
+                Todos
+            </h1>
+            {todos?.map((todo) => (
+                <div className={"todo " + (todo?.done && "line-through")} key={todo.id}>
+                    {todo.task}
+                    <button onClick={() => markDone(todo.id)}>Mark done</button>
+                    <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                </div>
+            ))}
+            <form
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    insert(event.target.task.value);
+                }}
+            >
+                <input type="text" name="task" />
+                <button type="submit">New Todo</button>
+            </form>
         </div>
     );
 };
